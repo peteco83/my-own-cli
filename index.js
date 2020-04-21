a911da302a0062ce1e78b6e9c74010ed1d6e2949
peteco83 <peteco1983@outlook.com>
2020-04-21 13:51:34 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+// lo primero que hay hacer es lo de arriba para que se llame la funcion sin poner node 
+
+const fetch = require("node-fetch")
+
+
+const federico = () => {
+    console.log("My name is Federico")
+    console.log("My age is 36")
+    console.log("I live in Berlin")
+}
+
+federico()
